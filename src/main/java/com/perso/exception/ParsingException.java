29960438e0637ba146d630db18c9932fc4d443b0
Julien Férézou <jferezou@gmail.com>
2017-09-16 11:00:17 +0200

    esception

---
+++ src/main/java/com/perso/exception/ParsingException.java
@@ -0,0 +1,17 @@
+package com.perso.exception;
+
+public class ParsingException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+	public ParsingException() {
+        super();
+    }
+
+	  public ParsingException(String s) {
+        super(s);
+    }
+}
